e033f3fef5185f1918cf37b9878aced70b1727d0
aleynaguner <aleynaguner@hotmail.com>
2019-11-15 00:22:13 +0300

    Yazı Tura

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -9,14 +9,30 @@ class CoinFlipper extends Component {
     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
         side: "tura",
-        donuyor: false
+        donuyor: false,
+        atis : 0,
+        yaziMi : 0
     }
   }
   handleClick = () => {
+    //her tıklandığında atış sayısını bir arttırdım
+    this.setState({
+      atis : this.state.atis+1
+    });
+
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "donuyor" durumunu "true" yapıyoruz.
     this.setState({donuyor: true});
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "donuyor" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({donuyor: false}), 1000);
+    setTimeout(() => this.setState({
+      donuyor: false,
+      //yazı ve turaya karar vermek için 1 ya da 0 döndürmesi durumuna baktım
+      side: (Math.round(Math.random())=== 0  ? 'yazi' : 'tura')
+      }), 1000);
+
+      if(this.state.side === 'yazi'){
+        this.setState({yaziMi : this.state.yaziMi+1});
+      }
+
   };
 
   render(){
@@ -26,12 +42,12 @@ class CoinFlipper extends Component {
         <Coin side={this.state.side} donuyor={this.state.donuyor} />
         <button onClick={this.handleClick} >At!</button>
         <p>
-            Toplam
-            <strong> 5 </strong>
+            Toplam 
+            <strong> {this.state.atis} </strong> 
             atıştan
-            <strong> 3 </strong>
+            <strong> {this.state.atis - this.state.yaziMi} </strong>
             ü tura
-            <strong> 2 </strong>
+            <strong> {this.state.yaziMi} </strong>
             si yazı geldi.</p>
       </div>
     )
